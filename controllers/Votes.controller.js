8de3dff71283e8737a9dfef83eabfd6108daa0dc
Damian-luis <damian.luis.porta@gmail.com>
2023-01-11 23:36:57 -0300

    feat: validations added

--- controllers/Votes.controller.js
+++ controllers/Votes.controller.js
@@ -1,4 +1,5 @@
 const Person = require('../config/db.js')
+const { body, validationResult } = require('express-validator');
 module.exports={
     addVote:async(req,res)=>{
         const name=req.body.name
@@ -7,7 +8,11 @@ module.exports={
         const birthDate=req.body.birthDate
         const country=req.body.country
         const cb=req.body.cb
-        try{
+        const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ status:false,message:"Por favor rellene todos los campos requeridos para enviar su voto" });
+    }
+ try{ 
             await Person.add({
                 name,lastName,mail,birthDate,country,cb
             })
@@ -22,49 +27,49 @@ module.exports={
         try{
             const data=await Person.get()
             const list=data.docs.map(e=>{return e.data()})
-            var dataPerCountry={
-                "argentina":0,
-                "brasil":0,
-                "chile":0,
-                "colombia":0,
-                "mexico":0,
-                "peru":0,
-                "uruguay":0,
-                "venezuela":0
-            }
+            var dataPerCountry=[
+                {"argentina":0},
+                {"brasil":0},
+                {"chile":0},
+                {"colombia":0},
+                {"mexico":0},
+                {"peru":0},
+                {"uruguay":0},
+                {"venezuela":0}
+            ]
             for(let i=0; i<list.length;i++){
                 if(list[i].country==="argentina"){
-                    console.log("pasate")
-                    dataPerCountry.argentina=dataPerCountry.argentina+1
+                    dataPerCountry[0].argentina=dataPerCountry[0].argentina+1
                 }
                 if(list[i].country==="brasil"){
-                    dataPerCountry.brasil=dataPerCountry.brasil+1
+                    dataPerCountry[1].brasil=dataPerCountry[1].brasil+1
                 }
                 if(list[i].country==="chile"){
-                    dataPerCountry.chile=dataPerCountry.chile+1
+                    dataPerCountry[2].chile=dataPerCountry[2].chile+1
                 }
                 if(list[i].country==="colombia"){
-                    dataPerCountry.colombia=dataPerCountry.colombia+1
+                    dataPerCountry[3].colombia=dataPerCountry[3].colombia+1
                 }
                 if(list[i].country==="mexico"){
-                    dataPerCountry.mexico=dataPerCountry.mexico+1
+                    dataPerCountry[4].mexico=dataPerCountry[4].mexico+1
                 }
                 if(list[i].country==="peru"){
-                    dataPerCountry.peru=dataPerCountry.peru+1
+                    dataPerCountry[5].peru=dataPerCountry[5].peru+1
                 }
                 if(list[i].country==="uruguay"){
-                    dataPerCountry.uruguay=dataPerCountry.uruguay+1
+                    dataPerCountry[6].uruguay=dataPerCountry[6].uruguay+1
                 }
                 if(list[i].country==="venezuela"){
-                    dataPerCountry.venezuela=dataPerCountry.venezuela+1
+                    dataPerCountry[7].venezuela=dataPerCountry[7].venezuela+1
                 }
             }
+            
             res.status(200).json({
                 status:true,
                 message:"Votos conseguidos exitosamente",
                 list,
                 ammounVotes:list.length,
-                dataPerCountry
+                dataPerCountry:dataPerCountry
             })
         }
         catch(e){
--- routes/Routes.js
+++ routes/Routes.js
@@ -1,7 +1,15 @@
 const express=require('express')
 const Person=require("../controllers/Votes.controller.js")
 const Votes=express.Router()
-
-Votes.post("/addvote",Person.addVote)
+const { body, validationResult } = require('express-validator');
+var validator=[
+    body('name').exists().isLength({ min: 2 }).not().isEmpty(),
+    body('lastName').exists().isLength({ min: 2 }).not().isEmpty(),
+    body('mail').isEmail().not().isEmpty(),
+    body('birthDate').not().isEmpty().not().isEmpty(),
+    body('country').not().isEmpty().isLength({ min: 2 }).not().isEmpty(),
+    body('cb').exists()
+]
+Votes.post("/addvote",validator,Person.addVote)
 Votes.get("/getvotes",Person.getResults)
 module.exports =Votes
